ce90c56c4853d250e16495968d82976d1c6fd0d4
riekuwa <riekuwa1126@gmail.com>
2016-04-29 19:06:51 +0900

    マイページ作成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,15 +1,17 @@
 class TweetsController < ApplicationController
   before_action :move_to_index, except: :index
   def index
-    @tweets = Tweet.all.order("created_at DESC").page(params[:page]).per(5)
+    @tweets = Tweet.includes(:user).order("created_at DESC").page(params[:page]).per(5)
   end
 
   def new
   end
 
   def create
-    Tweet.create(tweet_params)
+    Tweet.create(name: tweet_params[:name], text: tweet_params[:text], user_id: current_user.id)
   end
+
+
 
   private
   def tweet_params
